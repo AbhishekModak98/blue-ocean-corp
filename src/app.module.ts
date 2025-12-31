26962868132c4fc266b7c4f8b2516a2c07b4cbf0
Abhishek Modak <modakabhishek245@gmail.com>
2025-12-31 14:47:40 +0530

    mongodb connection succesfull and assignment running

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { CategoryModule } from './category/category.module';
 import { SubCategoryModule } from './sub-category/sub-category.module';
 import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [CategoryModule, SubCategoryModule, CourseModule]
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://abhishekrameshwarmodak_db_user:7TJzDP5fhYMM0j7V@assignment.pxb7cru.mongodb.net/?appName=Assignment`,
+      {
+        connectionFactory: (connection) => {
+          console.log('Connected to database');
+          return connection;
+        }
+      },
+    ),
+    CategoryModule,
+    SubCategoryModule,
+    CourseModule
+  ]
 })
 export class AppModule {}
